73b604e0cbc52cb7f98674fea134400262413ae0
KienKong <warriorproK@gmail.com>
2025-06-19 22:38:37 -0400

    added UI/UX to mobile

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,14 @@
+import SafeScreen from "@/components/SafeScreen";
 import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeScreen>
+    </ClerkProvider>
+  )
 }
