4fc4f18de166dc97fa29be3aa898c8e288ecb30d
KienKong <warriorproK@gmail.com>
2025-06-19 23:44:44 -0400

    added hooks, functionality for connecting with backend

---
+++ mobile/hooks/useTransactions.js
@@ -0,0 +1,71 @@
+import { useState, useCallback } from "react"
+const API_URL = "http://localhost:5001/api"
+import { Alert } from "react-native"
+
+export const useTransactions = (userId) => {
+  const [transactions, setTransactions] = useState([])
+  const [summary, setSummary] = useState({
+    balance : 0,
+    income : 0,
+    expense : 0,
+  })
+  const [isLoading, setLoading] = useState(true)
+
+  const fetchTransactions = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${userId}`)
+      const data = await response.json()
+      setTransactions(data)
+    } catch (error) {
+      console.error('Error fetching transactions:', error)
+    }
+  }, [userId])
+
+  const fetchSummary = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/summary/${userId}`)
+      const data = await response.json()
+      setSummary(data)
+    } catch (error) {
+      console.error('Error fetching summary:', error)
+    }
+  }, [userId])
+
+  const loadData = useCallback(async () => {
+    if (!userId) return
+
+    setLoading(true)
+    try{
+      await Promise.all([fetchTransactions(), fetchSummary()])
+    } catch (error) {
+      console.error('Error loading transactions:', error)
+    } finally {
+      setLoading(false)
+    }
+  }, [fetchTransactions, fetchSummary, userId])
+  
+  const deleteTransaction = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${id}`, {
+        method : 'DELETE',
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to delete transaction')
+      }
+      loadData()
+      Alert.alert('Success', 'Transaction deleted successfully')
+    } catch (error) {
+      console.error('Error deleting transaction:', error)
+      Alert.alert('Error', error.message)
+    }
+  }
+
+  return {
+    transactions,
+    summary,
+    isLoading,
+    loadData,
+    deleteTransaction,
+  }
+}
